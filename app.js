d4eb295ff671506d4d7f1c6b07197dccb1f396b7
Paulo Filho <orbishistorymaps@gmail.com>
2024-07-28 17:48:12 +1200

    1.4.3
    First JavaScript code added

---
+++ app.js
@@ -0,0 +1,8 @@
+// ===== SCROLL TO TOP BUTTON =====
+
+let mybutton = document.getElementsByClassName("fa-solid fa-arrow-up");
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }

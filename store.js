55ffa1215b96f6ac656c5e561a1e9ff972e29b81
Paulo Filho <orbishistorymaps@gmail.com>
2024-09-09 17:38:54 +1200

    1.7.3
    1- Added more properties to objects of Shop List
    2- Added functionality to View More button

--- store.js
+++ store.js
@@ -13,10 +13,12 @@ const addToCart = document.getElementById("addToCart"); // Use getElementById
 // ===== Creating a Products Array ===== //
 
 class ShoppingItem {
-  constructor(name, resolution, format, price, description) {
+  constructor(image, name, category, specs, material, price, description) {
+    this.image = image;
     this.name = name;
-    this.resolution = resolution;
-    this.format = format;
+    this.category = category;
+    this.specs = specs;
+    this.material = material;
     this.price = price;
     this.description = description;
   }
@@ -27,41 +29,51 @@ class ShoppingItem {
 // ======================================== //
 
 const map1 = new ShoppingItem(
-  "Sumer & Akkad Map",
-  "7016 x 4961 pixels 300 dpi",
-  "A2 (420 x 594 mm)",
+  "imgs/shop-items/sumershop1.jpg",
+  "Sumer & Akkad",
+  "Artistic Map",
+  "A2 (420 x 594 mm) / 7016 x 4961 pixels 300 dpi",
+  "Fine Art Smooth Cotton Rag 330gsm Matte",
   350,
   "This is the Sumerian & Akkad Map"
 );
 
 const map2 = new ShoppingItem(
-  "Ancient Egypt and Kush Kingdoms Map",
-  "7016 x 4961 pixels 300 dpi",
-  "A3 (420 x 297 mm)",
+  "imgs/shop-items/egyptshop1.jpg",
+  "Ancient Egypt & Kush Kingdoms",
+  "Artistic Map",
+  "A3 (420 x 297 mm) / 7016 x 4961 pixels 300 dpi",
+  "Fine Art Smooth Cotton Rag 330gsm Matte",
   250,
   "This is the Ancient Egypt and Kush Kingdoms Map"
 );
 
 const map3 = new ShoppingItem(
-  "Clash of Worlds: Hittites vs Achaens Map",
-  "4961 x 7016 pixels 300 dpi",
-  "A3 (297 x 420 mm)",
+  "imgs/shop-items/hitshop1.jpg",
+  "Clash of Worlds: Hittites vs Achaens",
+  "Artistic Map",
+  "A3 (297 x 420 mm) / 4961 x 7016 pixels 300 dpi",
+  "Fine Art Smooth Cotton Rag 330gsm Matte",
   250,
   "This is the Clash of Worlds: Hittites vs Achaens Map"
 );
 
 const map4 = new ShoppingItem(
-  "Assyrian Empire Map",
-  "7016 x 4961 pixels 300 dpi",
-  "A2 (420 x 594 mm)",
+  "imgs/shop-items/assyriashop1.jpg",
+  "Assyrian Empire",
+  "Artistic Map",
+  "A2 (420 x 594 mm) / 7016 x 4961 pixels 300 dpi",
+  "Fine Art Smooth Cotton Rag 330gsm Matte",
   350,
   "This is the Assyrian Empire Map"
 );
 
 const map5 = new ShoppingItem(
-  "Aotearoa New Zealand Map",
-  "4961 x 7016 pixels 300 dpi",
-  "A2 (420 x 594 mm)",
+  "imgs/shop-items/nzshop1.jpg",
+  "Aotearoa New Zealand",
+  "Artistic Map",
+  "A2 (420 x 594 mm) / 4961 x 7016 pixels 300 dpi",
+  "Fine Art Smooth Cotton Rag 330gsm Matte",
   350,
   "This is the Aotearoa New Zealand Map"
 );
@@ -81,25 +93,43 @@ console.log(shoppingItems);
 const shopItemDisplay = document.getElementById("shop-item-display");
 
 // Use DOMContentLoaded event listener
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Your code to populate shopItemDisplay
   shoppingItems.forEach((item, index) => {
     // Create a new shop-item element for each item
     const shopItem = document.createElement("div");
     shopItem.classList.add("shop-item");
 
+    // Create divs for image and information
+    const shopGallery = document.createElement("div");
+    shopGallery.classList.add("shop-gallery");
+
+    const shopInfo = document.createElement("div");
+    shopInfo.classList.add("shop-info");
+
     // Create child elements for each property
+
+    const itemImage = document.createElement("img");
+    itemImage.classList.add("item-image");
+    itemImage.setAttribute("src", item.image);
+    
+
     const itemName = document.createElement("p");
     itemName.classList.add("item-name");
     itemName.textContent = `${item.name}`;
 
-    const itemResolution = document.createElement("p");
-    itemResolution.classList.add("item-resolution");
-    itemResolution.textContent = `Resolution: ${item.resolution}`;
+    const itemCategory = document.createElement("p");
+    itemCategory.classList.add("item-category");
+    itemCategory.textContent = `${item.category}`;
 
-    const itemFormat = document.createElement("p");
-    itemFormat.classList.add("item-format");
-    itemFormat.textContent = `Format: ${item.format}`;
+    const itemSpecs = document.createElement("p");
+    itemSpecs.classList.add("item-specs");
+    itemSpecs.textContent = `Specifications: ${item.specs}`;
+    itemSpecs.style.display = "none"; // Initially hidden
+
+    const itemMaterial = document.createElement("p");
+    itemMaterial.classList.add("item-material");
+    itemMaterial.textContent = `${item.material}`;
 
     const itemPrice = document.createElement("p");
     itemPrice.classList.add("item-price");
@@ -108,30 +138,46 @@ document.addEventListener('DOMContentLoaded', function() {
     const itemDescription = document.createElement("p");
     itemDescription.classList.add("item-description");
     itemDescription.textContent = item.description;   
-
+    itemDescription.style.display = "none"; 
 
     // Append child elements to the shopItem
-    shopItem.appendChild(itemName);
-    shopItem.appendChild(itemResolution);
-    shopItem.appendChild(itemFormat);
-    shopItem.appendChild(itemPrice);
-    shopItem.appendChild(itemDescription);
+    shopGallery.appendChild(itemImage);
+    shopInfo.appendChild(itemName);
+    shopInfo.appendChild(itemCategory);
+    shopInfo.appendChild(itemSpecs);
+    shopInfo.appendChild(itemMaterial);
+    shopInfo.appendChild(itemPrice);
+    shopInfo.appendChild(itemDescription);
 
     // Create buttons
     const viewMoreButton = document.createElement("button");
     viewMoreButton.classList.add("page-button");
     viewMoreButton.textContent = "View More";
-   
+
     const addToCartButton = document.createElement("button");
     addToCartButton.classList.add("page-button");
     addToCartButton.textContent = "Add to Cart";
-   
+
+        // Toggle display of detailed information on button click
+        viewMoreButton.addEventListener("click", function () {
+            if (itemSpecs.style.display === "none") {
+              itemSpecs.style.display = "block";
+              itemDescription.style.display = "block";
+              viewMoreButton.textContent = "View Less";
+            } else {
+              itemSpecs.style.display = "none";
+              itemDescription.style.display = "none";
+              viewMoreButton.textContent = "View More";
+            }
+          });
+
     // Append buttons to the shopItem
-    shopItem.appendChild(viewMoreButton);
-    shopItem.appendChild(addToCartButton);
-   
+    shopInfo.appendChild(viewMoreButton);
+    shopInfo.appendChild(addToCartButton);
+
     // Append the shopItem to the shopItemDisplay
-    shopItemDisplay.appendChild(shopItem);
+    shopItem.appendChild(shopGallery);
+    shopItem.appendChild(shopInfo);
 
     // Append the shopItem to the shopItemDisplay
     shopItemDisplay.appendChild(shopItem);
